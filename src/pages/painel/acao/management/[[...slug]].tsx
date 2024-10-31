56526305b639c67c42d5041a3b1097531f14626c
Douglas Pacor <douglas@epicquestti.com.br>
2024-10-31 18:18:42 -0300

    doing actions

--- src/pages/painel/acao/management/[[...slug]].tsx
+++ src/pages/painel/acao/management/[[...slug]].tsx
@@ -1,16 +1,90 @@
 import { PanelFrame } from "@/components"
-import { ArrowBackIosNew, BackHand, Save } from "@mui/icons-material"
+import security from "@/config/actions/security"
+import { useGlobalCtx } from "@/context/Global"
+import fetchApi from "@/lib/fetchApi"
+import { ArrowBackIosNew, Save } from "@mui/icons-material"
 import { Box, Button, Grid, Paper, TextField } from "@mui/material"
-import { NextPage } from "next"
+import { GetServerSideProps, NextPage } from "next"
 import { useRouter } from "next/router"
-import { useState } from "react"
+import { useContext, useState } from "react"
+type mo = "visualizing" | "creating" | ""
+type ic = "visibility" | "create" | ""
+type acaoManagement = {
+  locationIcon: ic
+  mode: mo
+}
 
-const acaoSelecionada: NextPage = () => {
+export const getServerSideProps: GetServerSideProps<
+  acaoManagement
+> = async context => {
+  const urlSlug = context.params?.slug
+
+  if (!urlSlug) {
+    return {
+      props: { locationIcon: "", mode: "" },
+      redirect: {
+        destination: "/painel/inicio?erro=2"
+      }
+    }
+  }
+
+  let icon: ic = ""
+  let mode: mo = ""
+
+  if (urlSlug[0] === "new") {
+    icon = "create"
+    mode = "creating"
+  } else {
+    icon = "visibility"
+    mode = "visualizing"
+  }
+
+  return {
+    props: { locationIcon: icon, mode: mode }
+  }
+}
+
+const acaoSelecionada: NextPage<acaoManagement> = props => {
+  const [name, setName] = useState("")
+  const [canonical, setCanonical] = useState("")
   const [alerMessage, setAlerMessage] = useState("")
   const [showAlert, setShowAlert] = useState(false)
   const [loading, setLoading] = useState(false)
   const router = useRouter()
-  const slug = router.query.slug
+  const globalContext = useGlobalCtx()
+
+  const saveAction = async () => {
+    try {
+      setLoading(true)
+
+      const apiResult = await fetchApi.post(
+        security.action.new,
+        {
+          name: name,
+          canonical: canonical
+        },
+        {
+          headers: {
+            authorization: globalContext.user?.credential
+          }
+        }
+      )
+
+      if (!apiResult.success) throw new Error(apiResult.message)
+
+      setLoading(false)
+      setAlerMessage(apiResult.message || "")
+      setShowAlert(true)
+
+      const newId = apiResult.data.id
+      router.push(`/painel/acao/management/${newId}`)
+    } catch (error: any) {
+      setLoading(false)
+      setAlerMessage(error.message)
+      setShowAlert(true)
+    }
+  }
+
   return (
     <PanelFrame
       alerMessage={alerMessage}
@@ -29,9 +103,14 @@ const acaoSelecionada: NextPage = () => {
           text: "Ações"
         },
         {
-          href: `/painel/acao/management/${1}`,
-          iconName: "visibility",
-          text: "Vizualizando ação"
+          href:
+            props.mode === "creating"
+              ? "/painel/acao/management/new"
+              : `/painel/acao/management/${
+                  router.query.slug ? router.query.slug[0] : ""
+                }`,
+          iconName: props.locationIcon,
+          text: props.mode === "creating" ? "Criando ação" : "Vizualizando ação"
         }
       ]}
     >
@@ -40,10 +119,30 @@ const acaoSelecionada: NextPage = () => {
           <Paper sx={{ padding: 2 }}>
             <Grid container spacing={2}>
               <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-                <TextField label="Nome" fullWidth />
+                <TextField
+                  label="Nome"
+                  fullWidth
+                  value={name}
+                  inputProps={{
+                    maxLength: 40
+                  }}
+                  onChange={e => {
+                    setName(e.target.value)
+                  }}
+                />
               </Grid>
               <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-                <TextField label="Nome canónico" fullWidth />
+                <TextField
+                  label="Nome canónico"
+                  fullWidth
+                  value={canonical}
+                  inputProps={{
+                    maxLength: 40
+                  }}
+                  onChange={e => {
+                    setCanonical(e.target.value)
+                  }}
+                />
               </Grid>
             </Grid>
           </Paper>
@@ -66,7 +165,7 @@ const acaoSelecionada: NextPage = () => {
             >
               Voltar
             </Button>
-            <Button variant="contained" endIcon={<Save />}>
+            <Button variant="contained" endIcon={<Save />} onClick={saveAction}>
               Salvar
             </Button>
           </Box>
