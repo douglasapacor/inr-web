cdd6b8fa9de74f4e51a8095f08bc833d81e9c7a0
Douglas Pacor <douglas@epicquestti.com.br>
2024-12-09 18:53:11 -0300

    doing

--- src/pages/painel/tarefa/board/[id].tsx
+++ src/pages/painel/tarefa/board/[id].tsx
@@ -1,12 +1,9 @@
 import { PanelFrame } from "@/components"
-import theme from "@/styles/theme"
-import { Box, Grid, Icon, Paper, Typography } from "@mui/material"
+import { Add, Edit, MoreHoriz } from "@mui/icons-material"
+import { Box, Button, Grid, Icon, Paper, Typography } from "@mui/material"
 import { NextPage } from "next"
 import { useState } from "react"
 
-type boardType = {
-  name: string
-}
 
 const BoardContent: NextPage = () => {
   const [alerMessage, setAlerMessage] = useState("")
@@ -59,65 +56,62 @@ const BoardContent: NextPage = () => {
             <div id="TaskColumn">
               <Grid container spacing={2}>
                 <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                  <Box sx={{ width: "100%", padding: 1 }}>
-                    <Typography variant="body2">
-                      <strong>Título column</strong>
-                    </Typography>
+                  <Grid container spacing={1} sx={{ color: "white", fontSize: "10pt" }}>
+                    <Grid item xs={10} sm={10} md={10} lg={10} xl={10}>
+                      <Box sx={{ paddingLeft: 2 }}>Titulo</Box>
+                    </Grid>
+                    <Grid item xs={2} sm={2} md={2} lg={2} xl={2}>
+                      <Box sx={{ display: "flex", justifyContent: "center" }}>
+                        <div>
+                          <MoreHoriz fontSize="small" />
+                        </div>
+                      </Box>
+                    </Grid>
+                  </Grid>
+                </Grid>
+
+                <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                  <Box sx={{
+                    width: "100%",
+                    height: "70px",
+                    background: "#424242",
+                    borderRadius: 1,
+                  }}>
+                    <div style={{
+                      color: "white",
+                      fontFamily: "-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Noto Sans, Ubuntu, Droid Sans, Helvetica Neue, sans-serif",
+                      fontSize: "14px",
+                      fontWeight: "400",
+                      width: "100%",
+                      marginBottom: "4px",
+                      padding: "6px"
+                    }}>
+                      <Grid container spacing={2}>
+                        <Grid item xs={10} sm={10} md={10} lg={10} xl={10}>titulo card</Grid>
+                        <Grid item xs={2} sm={2} md={2} lg={2} xl={2}>
+                          <div>
+                            <MoreHoriz fontSize="small" />
+                          </div>
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}></Grid>
+                      </Grid>
+                    </div>
                   </Box>
                 </Grid>
+
                 <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                  <Paper sx={{ minHeight: 100 }} elevation={6}>
-                    <Grid container>
-                      <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                        <Box
-                          sx={{
-                            width: "100%",
-                            paddingTop: 1,
-                            paddingLeft: 1
-                          }}
-                        >
-                          <Typography variant="body2">Título card</Typography>
-                        </Box>
-                      </Grid>
-                      <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                        <Box
-                          sx={{
-                            width: "100%",
-                            padding: 1
-                          }}
-                        >
-                          <Box
-                            sx={{
-                              width: "100%",
-                              minHeight: "80px",
-                              background: "#E0E0E0",
-                              borderRadius: 1,
-                              wordWrap: "break-word",
-                              whiteSpace: "normal",
-                              p: 0.5
-                            }}
-                          >
-                            asdasd asd asdasds sad ad asdasdas asd ad
-                          </Box>
-                        </Box>
-                      </Grid>
-                    </Grid>
-                  </Paper>
-                </Grid>
-                <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                  <div id="TaskButtonAddCard">
-                    <Icon fontSize="large">add_circle_outline</Icon>
-                  </div>
+                  <Button sx={{ color: "white" }} variant="text" fullWidth startIcon={<Add />}>Adicionar Cartão</Button>
                 </Grid>
               </Grid>
             </div>
+
             <div id="TaskColumnAdd">
-              <Icon fontSize="large">add_circle_outline</Icon>
+              <Button sx={{ color: "white" }} variant="text" fullWidth startIcon={<Add />}>Adicionar Lista</Button>
             </div>
           </div>
         </Grid>
       </Grid>
-    </PanelFrame>
+    </PanelFrame >
   )
 }
 
